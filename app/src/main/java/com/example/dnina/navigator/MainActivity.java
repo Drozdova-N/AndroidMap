39e4aa1523e69a93f3ba8a6bd586ca76dd348740
dnina <quiet012@mail.ru>
2019-04-02 18:21:52 +0300

    v0.1

--- app/src/main/java/com/example/dnina/navigator/MainActivity.java
+++ app/src/main/java/com/example/dnina/navigator/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.dnina.navigator;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GoogleMap map;
+
+    private GridLayout gridLayout;
+
+    private Button btnGO;
+
+    private ConstraintLayout constraintLayout;
+    private ImageView imageDownArrow ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridLayout = findViewById(R.id.grid_header);
+        btnGO = findViewById(R.id.btn_GO);
+
+        imageDownArrow = findViewById(R.id.down_arrow);
+        // отрисовка карты
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_View);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+            }
+        });
+
+
+        btnGO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               imageDownArrow.setVisibility(View.INVISIBLE);
+            }
+        });
+
     }
+
+
 }
