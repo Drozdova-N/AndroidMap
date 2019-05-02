768b0163b9d58cc7b33cf52f33323a71be4593bb
dnina <quiet012@mail.ru>
2019-05-03 01:35:55 +0300

    new Version input A and B

--- app/src/main/java/com/example/dnina/navigator/MapView.java
+++ app/src/main/java/com/example/dnina/navigator/MapView.java
@@ -3,6 +3,8 @@ package com.example.dnina.navigator;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -10,27 +12,43 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.List;
 
 public class MapView {
 
-    private GoogleMap map ;
+    private GoogleMap map;
+    private Marker markerStart;
+    private Marker markerFinish;
 
-    public  MapView(GoogleMap map)
-    {
-        this.map = map;
+//    private static MarkerOptions start = new MarkerOptions();
+//    private static MarkerOptions finish = new MarkerOptions();
+//    private static PolylineOptions polyline = new PolylineOptions();
+
+    private static Polyline polylineFinal;
+
+    public MapView(GoogleMap googleMap) {
+        map = googleMap;
     }
 
     public GoogleMap getMap() {
+
         return map;
     }
 
-    public  void drawRoute(List<LatLng> listPoint , int size)
-    {
+    public void drawRoute(List<LatLng> listPoint, int size) {
+
+        if (polylineFinal != null) polylineFinal.remove();
+        if (markerStart != null && markerFinish != null) {
+            markerStart.remove();
+            markerFinish.remove();
+        }
+
+
         PolylineOptions polyline = new PolylineOptions();
-        polyline.width(4f).color(0xffab274f);
+        polyline.width(12f).color(0xffab274f);
         LatLngBounds.Builder llBounds = new LatLngBounds.Builder();
 
         for (int i = 0; i < listPoint.size(); i++) {
@@ -40,18 +58,18 @@ public class MapView {
 
         }
 
-        MarkerOptions start = new MarkerOptions().position(listPoint.get(1)).title("A");
+        MarkerOptions start = new MarkerOptions().position(listPoint.get(0)).title("A");
         MarkerOptions finish = new MarkerOptions().position(listPoint.get(listPoint.size() - 1)).title("B");
-        map.addMarker(start);
-        map.addMarker(finish);
-        map .addPolyline(polyline);
+
+        markerStart = map.addMarker(start);
+        markerFinish = map.addMarker(finish);
+        polylineFinal = map.addPolyline(polyline);
         LatLngBounds latLngBounds = llBounds.build();
-        CameraUpdate track = CameraUpdateFactory.newLatLngBounds(latLngBounds, size, size, 25) ;
+        CameraUpdate track = CameraUpdateFactory.newLatLngBounds(latLngBounds, size, size, 25);
         map.moveCamera(track);
     }
 
-    public void putMarker(LatLng ll)
-    {
+    public void putMarker(LatLng ll, boolean myImage) {
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(ll, 15));
 
         CameraPosition cameraPosition = new CameraPosition.Builder()
@@ -65,13 +83,16 @@ public class MapView {
 
         // Add Marker to Map
         MarkerOptions option = new MarkerOptions();
-        BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.cat2);
-        //   option.setIcon(bitmapDescriptor);
-        option.title("My Location");
         option.snippet(ll.toString());
         option.position(ll);
         Marker currentMarker = map.addMarker(option);
+        if (myImage) {
+            option.title("My Location");
+            BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.cat2);
+            currentMarker.setIcon(bitmapDescriptor);
+        }
         currentMarker.showInfoWindow();
-        currentMarker.setIcon(bitmapDescriptor);
+
     }
+
 }
